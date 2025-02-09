5cff7286f4dea2169b4856d08f62aba85affe708
Frank Pizzella <fpizz210@gmail.com>
2025-02-09 00:10:09 -0500

    BJN-19: Added hot reloads for react in docker

--- Frontend/BlackJackNow/src/App.tsx
+++ Frontend/BlackJackNow/src/App.tsx
@@ -41,7 +41,7 @@ function App() {
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
-        <h1>Vite + React</h1>
+        <h1>Vite + React + LIVE!🔥</h1>
         <div className="card">
           {/* <RoomDisplay /> */}
           <SocketMessage />
